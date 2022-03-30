50bf19d2abefbfcbe361fd546333e3ccc15421ce
Leonardo Munoz <leonardo.munoz@easypayfinance.com>
2022-03-30 11:58:51 -0600

    add index.js file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
